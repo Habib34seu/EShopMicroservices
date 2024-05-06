3dc95dad375e3c9b2f7582a068eef96634966a8b
Habib34seu <34seu.habib@gmail.com>
2024-05-07 01:42:31 +0600

    update

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -1,5 +1,15 @@
-﻿namespace Basket.API.Basket.GetBasket;
+﻿
+namespace Basket.API.Basket.GetBasket;
 
-public class GetBasketHandler
+public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+public record GetBasketResult(ShoppingCart Cart);
+internal class GetBasketQueryHandler 
+    
+    : IQueryHandler<GetBasketQuery, GetBasketResult>
 {
+    public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+    {
+
+        return new GetBasketResult(new ShoppingCart("swm"));
+    }
 }
