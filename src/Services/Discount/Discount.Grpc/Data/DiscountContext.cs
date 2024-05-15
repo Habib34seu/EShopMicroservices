039c1843220302665c7b1734b17482028c61cbe4
Habib34seu <34seu.habib@gmail.com>
2024-05-16 00:41:46 +0600

    grpc

---
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -0,0 +1,23 @@
+﻿using Discount.Grpc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data;
+
+public class DiscountContext : DbContext
+{
+    public DbSet<Coupon> Coupones { get; set; } = default!;
+
+    public DiscountContext(DbContextOptions<DiscountContext> options) : base(options)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Coupon>().HasData(
+            new Coupon { Id = 1, ProductName = "IPhone X", Description = "IPhone X Description", Amount = 500 },
+            new Coupon { Id = 2, ProductName = "Sumsung 10", Description = "Sumsung Description", Amount = 500 }
+            );
+    }
+
+}
