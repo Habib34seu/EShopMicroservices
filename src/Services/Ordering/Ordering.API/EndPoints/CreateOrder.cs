6b49064966fdb47b6419fb999ad9241fd02e0bee
Habiseub <34seu.habib@gmail.com>
2024-05-31 02:17:24 +0600

    update

---
+++ src/Services/Ordering/Ordering.API/EndPoints/CreateOrder.cs
@@ -0,0 +1,31 @@
+﻿using Ordering.Application.Orders.Commands.CreateOrder;
+
+namespace Ordering.API.EndPoints;
+
+/*
+ * Accepts a CreateOrderRequest object.
+ * Maps the request to a CreateOrderCommand
+ * Uses MediatR to sen the command to the corresponding handler.
+ * Returns a response with the created order's ID.
+ */
+
+public record CreateOrderRequest(OrderDto Order);
+public record CreateOrderRespose(Guid Id);
+public class CreateOrder : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/orders", async (CreateOrderRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<CreateOrderCommand>();
+            var result = await sender.Send(command);
+            var response = result.Adapt<CreateOrderRespose>();
+            return Results.Created($"/orders/{response.Id}",response);
+        })
+        .WithName("CreateOrder")
+        .Produces<CreateOrderRespose>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Create Order")
+        .WithDescription("Create Order"); 
+    }
+}
---
+++ src/Services/Ordering/Ordering.API/EndPoints/GetOrders.cs
@@ -0,0 +1,34 @@
+﻿using BuildingBlocks.Pagination;
+using Ordering.Application.Orders.Queries.GetOrders;
+
+
+namespace Ordering.API.EndPoints;
+
+
+/*
+ * Accepts pagination parameters.
+ * Constructs a GetOrdersQuery with thes parameters.
+ * Retrieves the data and returns it in a paginated formet.
+ */
+
+//public record GetOrdersRequest(PaginationRequest PaginationRequest);
+
+public record GetOrdersResponse(PaginatedResult<OrderDto> OrderDtos);
+public class GetOrders : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/orders", async ([AsParameters] PaginationRequest request, ISender sender) =>
+        {
+            var result = await sender.Send(new GetOrdersQuery(request));
+            var response = result.Adapt<GetOrdersResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("GetOrders")
+        .Produces<GetOrdersResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Get Orders")
+        .WithDescription("Get Orders");
+    }
+}
