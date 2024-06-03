3335fc103c65ac7fa387488885d734738a7f7f40
Habiseub <34seu.habib@gmail.com>
2024-06-03 23:56:23 +0600

    update

---
+++ src/Services/Basket/Basket.API/Basket/CheckoutBasket/CheckoutBasketHandler.cs
@@ -0,0 +1,44 @@
+﻿
+using BuildingBlocks.Messaging.Events;
+using MassTransit;
+
+namespace Basket.API.Basket.CheckoutBasket;
+
+public record CheckoutBasketCommand(BasketCheckoutDto CheckoutBasketDto)
+    : ICommand<CheckoutBasketResult>;
+public record CheckoutBasketResult(bool IsSuccess);
+
+public class CheckoutBasketCommandValidator : AbstractValidator<CheckoutBasketCommand>
+{
+    public CheckoutBasketCommandValidator()
+    {
+        RuleFor(x => x.CheckoutBasketDto).NotNull().WithMessage("CheckoutBasketDto can't be null");
+        RuleFor(x => x.CheckoutBasketDto.UserName).NotEmpty().WithMessage("UserName is required");
+    }
+}
+public class CheckoutBasketCommandHandler
+    (IBasketRepository repository, IPublishEndpoint publishEndpoint)
+    : ICommandHandler<CheckoutBasketCommand, CheckoutBasketResult>
+{
+    public async Task<CheckoutBasketResult> Handle(CheckoutBasketCommand command, CancellationToken cancellationToken)
+    {
+        /*
+         * get existing basket with total price
+         * set totalprice on basketchekout event message
+         * send basket checkout event to rabbitmq using masstransit
+         * delete the basket
+         */
+
+        var basket = await repository.GetBasket(command.CheckoutBasketDto.UserName, cancellationToken);
+        if (basket == null)  return new CheckoutBasketResult(false);
+
+        var eventMessage = command.CheckoutBasketDto.Adapt<BasketCheckoutEvent>();
+        eventMessage.TotalPrice = basket.TotalPrice;
+
+        await publishEndpoint.Publish(eventMessage,cancellationToken);
+
+        await repository.DeleteBasket(command.CheckoutBasketDto.UserName, cancellationToken);
+
+        return new CheckoutBasketResult(true);
+    }
+}
